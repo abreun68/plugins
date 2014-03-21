e7fb4f43c2f847cb3ff7ed2218c6b3766e7fa1f8
nabreu <nabreu@mse-f10ydx1.njmrsnet-aegis.com>
2014-03-21 14:47:11 -0400

    Java class related to the Pert Chart questions. In my opinion, the QuestionBase abstract class should simply be an interface, since no behaviour is being define in it, that could be used by the class extending it.

---
+++ Project1/src/DataAnalysis/PertChartQuestion.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAnalysis;
+
+import DataAnalysis.QuestionBase;
+import DataObject.EvalObject;
+import DataObject.Question;
+
+
+/**
+ * Pert Chart Plugin implementation
+ * @author Nacer Abreu
+ */
+public class PertChartQuestion extends QuestionBase {
+
+    public PertChartQuestion(EvalObject eval) {
+    }
+
+    @Override
+    public Question finMax() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Question findMin() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Question findOutlier() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Question findRecomendation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Question exploration() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String modifyQuestion(String s, int num, String l) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+}
