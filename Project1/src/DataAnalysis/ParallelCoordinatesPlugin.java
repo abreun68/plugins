7d9460aaf482584302868ef04e38c2316ecfabc7
nabreu <nabreu@mse-f10ydx1.njmrsnet-aegis.com>
2014-03-24 14:42:37 -0400

    Modified the plugin to reflect my latest understanding of it. They basically append specific questions to the visualization.

--- Project1/src/DataAnalysis/ParallelCoordinatesPlugin.java
+++ Project1/src/DataAnalysis/ParallelCoordinatesPlugin.java
@@ -17,7 +17,11 @@ public class ParallelCoordinatesPlugin implements Plugin {
         this.eval = eval;
         this.questions = eval.getQuestions();
         this.index = 0;
-
+        
+        append("Is there a pattern to the visualization?");
+        append("Was there an event that caused this?");
+        append("Does the number of entries make the visualization harder to interpret?");
+        append("Does the number of columns make the visualization harder to interpret?");
     }
 
     @Override
@@ -71,12 +75,12 @@ public class ParallelCoordinatesPlugin implements Plugin {
         // question is return.
 
         Question obj = null;
-        if (eval.getEvalType().equalsIgnoreCase("recommendation")) {
-            if ((this.index >= 0) && (index < questions.size())) {
-                obj = questions.get(this.index);
-                this.index++;
-            }
+
+        if ((this.index >= 0) && (index < questions.size())) {
+            obj = questions.get(this.index);
+            this.index++;
         }
+
         return obj;
     }//end of findRecomendation()
 
@@ -86,12 +90,12 @@ public class ParallelCoordinatesPlugin implements Plugin {
         // "exploration" question. It will return null, after the last 
         // question is return.
         Question obj = null;
-        if (eval.getEvalType().equalsIgnoreCase("exploration")) {
-            if ((this.index >= 0) && (this.index < questions.size())) {
-                obj = questions.get(this.index);
-                this.index++;
-            }
+
+        if ((this.index >= 0) && (this.index < questions.size())) {
+            obj = questions.get(this.index);
+            this.index++;
         }
+
         return obj;
     }//end of exploration()
 
@@ -110,7 +114,21 @@ public class ParallelCoordinatesPlugin implements Plugin {
     public int size() {
         return questions.size();
     }
-    
+
+    /**
+     * This method will append to the list of "canned" questions, the ones
+     * particular to this plugin. This method makes the following assumptions:
+     * 1. The time suggested length of question is 30 seconds 2. The type of
+     * answer is always a string. 3. The question is always rated as 0 (zero)
+     */
+    private void append(String question) {
+        int time = 30;
+        int score = 0;
+        String type = "s";
+        Question obj = eval.buildQuestion(question, time, type, score);
+        questions.add(obj);
+
+    }
     
     private Evaluation eval;
     private ArrayList<Question> questions;
