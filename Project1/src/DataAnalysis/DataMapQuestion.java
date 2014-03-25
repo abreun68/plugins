f29cd2c78d306782bd74d75f383366d05ffc8b03
MIKE <MIKE@MIKE-PC>
2014-03-24 21:18:07 -0400

    Added empty question files.

---
+++ Project1/src/DataAnalysis/DataMapQuestion.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package DataAnalysis;
+
+import DataObject.EvalObject;
+import DataObject.Question;
+
+/**
+ * @author MIKE
+ *
+ */
+public class DataMapQuestion extends QuestionBase {
+
+	private EvalObject eval;
+	
+	/**
+	 * 
+	 */
+	public DataMapQuestion(EvalObject eval) {
+		this.eval = eval;
+	}
+
+	/* (non-Javadoc)
+	 * @see DataAnalysis.QuestionBase#finMax()
+	 */
+	@Override
+	Question finMax() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see DataAnalysis.QuestionBase#findMin()
+	 */
+	@Override
+	Question findMin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see DataAnalysis.QuestionBase#findOutlier()
+	 */
+	@Override
+	Question findOutlier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see DataAnalysis.QuestionBase#findRecomendation()
+	 */
+	@Override
+	Question findRecomendation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see DataAnalysis.QuestionBase#exploration()
+	 */
+	@Override
+	Question exploration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see DataAnalysis.QuestionBase#modifyQuestion(java.lang.String, int, java.lang.String)
+	 */
+	@Override
+	String modifyQuestion(String s, int num, String l) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Project1/src/DataAnalysis/SemanticNetworkQuestion.java
@@ -0,0 +1,50 @@
+package DataAnalysis;
+
+import DataObject.EvalObject;
+import DataObject.Question;
+
+public class SemanticNetworkQuestion extends QuestionBase {
+
+	private EvalObject eval;
+	
+	public SemanticNetworkQuestion(EvalObject eval) {
+		this.eval = eval;
+	}
+
+	@Override
+	Question finMax() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Question findMin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Question findOutlier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Question findRecomendation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	Question exploration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	String modifyQuestion(String s, int num, String l) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
