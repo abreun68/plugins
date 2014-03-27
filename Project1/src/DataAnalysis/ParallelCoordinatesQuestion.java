811cfd5f3846a4f44eb0b3f72762866dad4b2459
david gwalthney <davidf.gwalthney@gmail.com>
2014-03-27 17:32:46 -0400

    added comments

--- Project1/src/DataAnalysis/ParallelCoordinatesQuestion.java
+++ Project1/src/DataAnalysis/ParallelCoordinatesQuestion.java
@@ -3,6 +3,19 @@ package DataAnalysis;
 import DataObject.EvalObject;
 import DataObject.Question;
 
+/**
+ * Parallel Coordinates Plugin implementation.
+ * Parallel coordinates is a common way of visualizing high-dimensional 
+ * geometry and analyzing multivariate data.
+ * 
+ * To show a set of points in an n-dimensional space, a backdrop is drawn 
+ * consisting of n parallel lines, typically vertical and equally spaced. 
+ * A point in n-dimensional space is represented as a polyline with vertices 
+ * on the parallel axes; the position of the vertex on the ith axis corresponds 
+ * to the ith coordinate of the point.
+ * 
+ * @author Nacer Abreu
+ */
 public class ParallelCoordinatesQuestion extends QuestionBase {
 
 	private EvalObject eval;
@@ -78,9 +91,10 @@ public class ParallelCoordinatesQuestion extends QuestionBase {
 		return null;
 	}
 	
-	/**
+    /**
      * This method will append to the list of "canned" questions, the ones
-     * particular to this plugin. 
+     * particular to this plugin. This method makes the following assumption(s): 
+     * 1. The question is always rated as 0 (zero)
      */
     private void append(String question, int time, String type) {
         int score = 0;
